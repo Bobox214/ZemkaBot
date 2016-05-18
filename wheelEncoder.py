7daaaabab7ae8ad3cd5b898ce4114d54febe0344
Boris Boutillier <Boris.Boutillier@gmail.com>
2016-05-18 20:10:02 +0000

    WheelEncoder fix after testing

--- wheelEncoder.py
+++ wheelEncoder.py
@@ -1,3 +1,4 @@
+from boneHelpers import *
 class WheelEncoder(object):
 
 	def __init__(self,pinName):
@@ -5,15 +6,15 @@ class WheelEncoder(object):
 
 	def setup(self):
 		self.reset()
-		GPIO.setup(pinName,GPIO.IN)
-		GPIO.add_event_detect(pinName,GPIO.BOTH,callback=self._tickUpdate)
+		GPIO.setup(self.pinName,GPIO.IN)
+		GPIO.add_event_detect(self.pinName,GPIO.BOTH,callback=self._tickUpdate)
 
 	def reset(self):
 		self._ticks = 0
 
 	def getTicks(self):
 		return self._ticks
-	def _tickUpdate(self,channel)
+	def _tickUpdate(self,channel):
 		self._ticks += 1
 
 
